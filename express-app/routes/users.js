d9281cdb0e7c681f9085477644a4ab51a9623cb5
Wessel <wmj.vantilburg@student.avans.nl>
2022-02-02 17:09:48 +0100

    feat: add initial mongoDB package and connection

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const dbo = require('../services/database');
+ 
+router.route('/').get((req, res) => {
+    dbo.getDb().then(db => {
+      db.collection("users")
+        .find({}).limit(50)
+        .toArray(function (err, result) {
+          if (err) {
+            res.status(400).send("Error fetching users!");
+          } else {
+              console.log(result)
+              res.json(result);
+          }
+        });
+      }
+    );
+  })
+  .post((req, res) => {
+    dbo.getDb().then(db => {
+      db.collection("users")
+      .insertOne(req.body, function (err, result) {
+        if (err) {
+          res.status(400).send("Error inserting user!");
+        } else {
+          console.log('id', result.insertedId);
+          res.status(204).send();
+        }
+      });
+    });
+  });
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+module.exports = router; 
